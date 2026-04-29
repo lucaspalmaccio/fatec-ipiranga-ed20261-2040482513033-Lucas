ea0e60969fefd1715e1aabe25e551abd20726df1
Lucas Palmaccio <127799983+lucaspalmaccio@users.noreply.github.com>
2026-04-28 21:35:10 -0300

    Update arvore_binaria.c

--- ED-20261-Atividades-N2/ED-20261-Atividade-N2-1/arvore_binaria.c
+++ ED-20261-Atividades-N2/ED-20261-Atividade-N2-1/arvore_binaria.c
@@ -10,7 +10,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include "arvore_binaria.h"
 
 // Criacao e insercao (recursiva) na BST                                      
 No* criarNo(int valor) {
